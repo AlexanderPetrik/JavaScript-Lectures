c32abcf233ca0618218d06f73b0489cae73fc642
AlexanderPetrik <painkiller408@gmail.com>
2016-12-14 23:15:27 +0200

    Lekture_2 Operators slide 5

---
+++ js/main.js
@@ -0,0 +1,90 @@
+"use strict"; // строгий режим написания кода
+
+// alert("Hello world");
+
+// console.log("Hello console");
+
+var myPersic = "Fish";
+
+// debugger;
+
+// var MY_CONSTANT = "text";
+
+var myPersic = "Meat";
+
+// console.log(myPersic);
+
+// var object = {
+	// name: "Shuric",
+	// age: "21"
+// };
+
+// var arrey = ["friday", "sunday", myPersic];
+
+// console.log(arrey);
+// console.log(arrey[0]);
+
+// console.log(object);
+
+// console.log(object["name"]);
+// console.log(object.age);
+
+// object.age = 25;
+
+// console.log(object.age);
+
+// arrey["newParams"] = "text params";
+
+// console.log(arrey);
+
+// arrey[3] = "Buhlo";
+// console.log(arrey);
+
+// arrey.push(777);
+
+// // function is{};
+// // array is {};
+
+// var myFunc = function () {
+	// alert("axe, axe, axe");
+// }
+
+// myFunc();
+
+// myFunc.myProperty = "test";
+
+// console.log(myFunc.myProperty);
+
+ function drinking () {
+	 alert(drinking.drink);
+	 return "i dream" + drinking.drink;
+	}
+
+ drinking.drink = "BUHLOO!!!";
+
+// drinking();
+
+// console.log(drinking());
+
+// console.log("3 > 5", 3 > '5', null > undefined);
+// console.log(3 > '5');
+// console.log(null > undefined);
+// console.log(null == 0);
+// console.log(5 == '5');
+// console.log(5 === '5');
+
+// console.log(3 == 3 && 5 == 5);
+// console.log(3 == 5 && 5 == 5);
+// console.log(3 == 3 && 5 == 3);
+
+var day = "Friday";
+// console.log(day == "Friday" && drinking());
+// console.log(day == "Friday" || drinking());
+
+console.log(!"");
+console.log(!"ikik");
+console.log("ikik");
+console.log(!!"ikik");
+
+
+
